e4f2a0e39cef6bff241967cfdf0d5ff5561d9727
sravanthi <sravanthik07@gmail.com>
2021-06-21 09:35:06 +0530

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EMPTY, EmptyError } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,75 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-demo';
+  userName: string ;
+  resetFlag: boolean;
+  password1: string;
+  password2: string;
+  pwdFlag:boolean;
+  pwdString: number=0;
+  skillArr : string[] = [];
+  skill:string;
+
+  onAddSkill(){
+    this.skillArr.push(this.skill);
+  }
+
+  isKeyUP(event: Event){
+    //let inputText = (<HTMLInputElement> event.target).value;
+   // InputEvent inputEvent = (<HTMLInputElement> event.target).
+    if(this.userName == ''){
+      this.resetFlag = true;
+    }
+    else{
+      this.resetFlag = false;
+    }
+  }
+
+  onPassword()
+  {
+    if(this.password1 == '' || this.password1.length < 8){
+      this.pwdFlag = false;
+    }
+    else{
+      this.pwdFlag=true;
+    }
+  }
+  onClick(event: Event){
+      this.userName = "";
+      this.resetFlag = true;
+      this.password1="";
+      this.password2="";
+      this.pwdFlag=false;
+      this.pwdString=0;
+  }
+
+  onChangePwd(){
+    if(this.password2 == ''){
+      this.pwdString = 0;
+    }
+    else if(this.password2.length < 8){
+      this.pwdString = 1;
+    }
+    else{
+      this.pwdString = 2;
+    }
+  }
+
+  getColor(){
+    if(this.pwdString == 0){
+      return 'yellow';
+    }
+    else if(this.pwdString == 1){
+      return 'red';
+    }
+    else{
+      return 'green';
+    }
+  }
+
+  
 }
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { FormsModule } from '@angular/forms';
+import { BackgroundComponent } from './background/background.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    BackgroundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
